71f5443c9ba032d6cda117c9131f633578bc7262
tosht0sh <manglanitoshith@gmail.com>
2026-03-18 14:49:59 +0100

    imu reading added to pose estimation

--- packages/wheel_utils/src/pose_from_wheel_encoder.py
+++ packages/wheel_utils/src/pose_from_wheel_encoder.py
@@ -7,6 +7,7 @@ from duckietown.dtros import DTROS, NodeType
 from duckietown_msgs.msg import WheelEncoderStamped, Pose2DStamped, Twist2DStamped
 from std_msgs.msg import Float64MultiArray, String
 from sensor_msgs.msg import Imu
+import numpy as np
 
 # laptop - 192.168.1.197
 
@@ -49,6 +50,9 @@ class WheelEncoderReaderNode(DTROS):
         )
         self.pose_pub = rospy.Publisher(f"/{self._vehicle_name}/pose_reader", Float64MultiArray, queue_size=10)
 
+        self.rate = 30
+        self.dt = 1 / self.rate
+
         # Geometry/calibration (override via ROS params if needed).
         self._wheel_radius = 0.0318
         self._axis_length = 0.1
@@ -58,7 +62,6 @@ class WheelEncoderReaderNode(DTROS):
 
         self.right_enc_resolution = None
         self.right_meters_per_tick = None
-
 
         self.x = 0.0
         self.y = 0.0
@@ -72,25 +75,12 @@ class WheelEncoderReaderNode(DTROS):
         self.k_r = 1.0
 
         # node for pose from kinematics node
-        self._vel_to_pose_topic = f"/{self._vehicle_name}/velocity_to_pose_node/pose"
-        self.vel_to_pose_node = rospy.Subscriber(
-            self._vel_to_pose_topic,
-            Pose2DStamped,
-            self.callback_vel_to_pose
-        )
-
-        self.vtp_x = 0.0
-        self.vtp_y = 0.0
-        self.vtp_theta = 0.0
-
-        self.vtp_x_actual = 0.0
-        self.vtp_y_actual = 0.0
-        self.vtp_theta_actual = 0.0
-
-        self.vtp_x_origin = None
-        self.vtp_y_origin = None
-        self.vtp_theta_origin = None
-
+        # self._vel_to_pose_topic = f"/{self._vehicle_name}/velocity_to_pose_node/pose"
+        # self.vel_to_pose_node = rospy.Subscriber(
+        #     self._vel_to_pose_topic,
+        #     Pose2DStamped,
+        #     self.callback_vel_to_pose
+        # )
 
         # imu settings
         self._imu_reader_topic = f"/duck1/imu_node/data"
@@ -98,22 +88,48 @@ class WheelEncoderReaderNode(DTROS):
         self._ang_vel = None
 
         self._imu_subscirber = rospy.Subscriber(self._imu_reader_topic, Imu, self.callback_imu)
+        # self._imu_rate = 30
 
         # kinematics node pose 
         self._vel_reader_topic = f"/duck1/kinematics_node/velocity"
 
-        self._lin_vel_kn = None
-        self._ang_vel_kn = None
+        self._lin_vel_kn = 0.0
+        self._ang_vel_kn = 0.0
 
         self._kin_node_subscirber = rospy.Subscriber(self._vel_reader_topic, Twist2DStamped, self.callback_kn)
+
+        # complementary filter setup for yaw
+        self._yaw_alpha = 0.97
+        self._yaw_bias = 0.0                                                            # TODO: make this dynamic
+        self._bias_samples = []
+        self._bias_ready = False
+
 
     def callback_kn(self, msg):
         self._lin_vel_kn = msg.v
         self._ang_vel_kn = msg.omega
 
+    # def callback_imu(self, msg):
+    #     self._ang_vel = msg.angular_velocity.z
+    #     self._ang_vel = self._ang_vel - self._yaw_bias
+    #     # TODO: maybe add a low psss filter here?
+
+    #     self.gyro_z = self.gyro_z_prev + (self._ang_vel / self._imu_rate)
+    #     self.gyro_z_prev = self.gyro_z
 
     def callback_imu(self, msg):
-        self._ang_vel = msg.angular_velocity.z
+        wz = msg.angular_velocity.z
+
+        if not self._bias_ready:    
+            self._bias_samples.append(wz)
+            # rospy.loginfo(self._bias_samples)
+
+            if len(self._bias_samples) >= 100:
+                self._yaw_bias = sum(self._bias_samples) / len(self._bias_samples)
+                self._bias_ready = True
+                rospy.loginfo(f"Estimated gyro bias: {self._yaw_bias:.6f} rad/s")
+        
+        self._ang_vel = wz - self._yaw_bias
 
     def callback_left(self, data):
         rospy.loginfo_once(f"Left encoder resolution: {data.resolution}")
@@ -131,24 +147,9 @@ class WheelEncoderReaderNode(DTROS):
             self.right_enc_resolution = data.resolution
             self.right_meters_per_tick = (2 * math.pi * self._wheel_radius) / self.right_enc_resolution
 
-    def callback_vel_to_pose(self, data):
-        self.vtp_x_actual = data.x
-        self.vtp_y_actual = data.y
-        self.vtp_theta_actual = data.theta
-
-        if self.vtp_x_origin is None:
-            self.vtp_x_origin = data.x
-            self.vtp_y_origin = data.y
-            self.vtp_theta_origin = data.theta
-
-        self.vtp_x = self.vtp_x_actual - self.vtp_x_origin
-        self.vtp_y = self.vtp_y_actual - self.vtp_y_origin
-        self.vtp_theta = self.vtp_theta_actual - self.vtp_theta_origin
-        self.vtp_theta = (self.vtp_theta + math.pi) % (2 * math.pi) - math.pi
-
     def position_calc(self):
 
-        if self._ticks_left is None or self._ticks_right is None:
+        if self._ticks_left is None or self._ticks_right is None or self._ang_vel is None or self._ang_vel is None or not self._bias_ready:
             return
 
         # previous tick 
@@ -160,7 +161,6 @@ class WheelEncoderReaderNode(DTROS):
         # update delta for current ticks
         dl = (self._ticks_left - self.prev_l) * self.left_meters_per_tick * self.k_l
         dr = (self._ticks_right - self.prev_r) * self.right_meters_per_tick * self.k_r
-
         # dl = (self._ticks_left - self.prev_l) * self.left_meters_per_tick
         # dr = (self._ticks_right - self.prev_r) * self.right_meters_per_tick 
 
@@ -168,11 +168,28 @@ class WheelEncoderReaderNode(DTROS):
         d = (dr + dl) / 2
         dtheta = (dr - dl) / self._axis_length
 
-        self.x += d * math.cos(self.theta + dtheta / 2)
-        self.y += d * math.sin(self.theta + dtheta / 2)
-        self.theta += (dtheta * 1.85)
-        # Keep heading bounded to [-pi, pi] for symmetric CW/CCW comparison.
-        self.theta = (self.theta + math.pi) % (2 * math.pi) - math.pi
+        theta_enc = self.theta + dtheta
+        # wrap to bound to [-pi, pi]
+        theta_enc = (theta_enc + math.pi) % (2 * math.pi) - math.pi
+
+        theta_gyro = self.theta + self._ang_vel * self.dt
+        # wrap to bound to [-pi, pi]
+        theta_gyro = (theta_gyro + math.pi) % (2 * math.pi) - math.pi
+
+        theta_new = self._yaw_alpha * theta_gyro + (1.0 - self._yaw_alpha) * theta_enc
+        # wrap to bound to [-pi, pi]
+        theta_new = (theta_new + math.pi) % (2 * math.pi) - math.pi
+
+        # midpoint position update
+        theta_mid = self.theta + 0.5 * (theta_new - self.theta)
+        theta_mid = (theta_mid + math.pi) % (2 * math.pi) - math.pi
+
+        # update x and y based on complemntary filter theta
+        self.x += d * math.cos(self.theta)
+        self.y += d * math.sin(self.theta)
+
+        # update theta
+        self.theta = theta_new
 
         # update previous ticks values
         self.prev_l = self._ticks_left
@@ -180,7 +197,7 @@ class WheelEncoderReaderNode(DTROS):
 
         
     def run(self):
-        rate = rospy.Rate(10)
+        rate = rospy.Rate(self.rate)
 
         while not rospy.is_shutdown():
 
@@ -189,24 +206,23 @@ class WheelEncoderReaderNode(DTROS):
 
                 self.position_calc()
 
-                msg = (
-                    f"Encoder pose [x, y, theta]: "
-                    f"{self.x:.3f}, {self.y:.3f}, {self.theta:.3f} | "
-                    # f"Velocity pose (relative) [x, y, theta]: "
-                    # f"{self.vtp_x:.3f}, {self.vtp_y:.3f}, {self.vtp_theta:.3f}"
-                    # f"Angular Velocity (x,y,z): "
-                    # f"({self._ang_vel.x:.3f}, {self._ang_vel.y:.3f}, {self._ang_vel.z:.3f}) | "
-                    # f"Linear Acceleration (x,y,z): "
-                    # f"({self._lin_acc.x:.3f}, {self._lin_acc.y:.3f}, {self._lin_acc.z:.3f})"
-                    f"Imu (angular velocity): "
-                    f"({self._ang_vel:.3f}) |"
-                    f"Kinematics Node (v, omega): "
-                    f"[{self._lin_vel_kn:.3f}, {self._ang_vel_kn:.3f}]"
-                    
-                )
-                rospy.loginfo(msg)
-                pose_msg = Float64MultiArray(data=[round(self.x, 3), round(self.y, 3), round(self.theta, 3)])
-                self.pose_pub.publish(pose_msg)
+                if self._bias_ready:
+                    msg = (
+                        f"Encoder pose [x, y, theta]: "
+                        f"{self.x:.3f}, {self.y:.3f}, {self.theta:.3f} | "
+                        # f"Angular Velocity (x,y,z): "
+                        # f"({self._ang_vel.x:.3f}, {self._ang_vel.y:.3f}, {self._ang_vel.z:.3f}) | "
+                        # f"Linear Acceleration (x,y,z): "
+                        # f"({self._lin_acc.x:.3f}, {self._lin_acc.y:.3f}, {self._lin_acc.z:.3f})"
+                        f"Imu (angular velocity): "
+                        f"({self._ang_vel:.3f}) |"
+                        f"Kinematics Node (v, omega): "
+                        f"[{self._lin_vel_kn:.3f}, {self._ang_vel_kn:.3f}]"
+                        
+                    )
+                    rospy.loginfo(msg)
+                    pose_msg = Float64MultiArray(data=[round(self.x, 3), round(self.y, 3), round(self.theta, 3)])
+                    self.pose_pub.publish(pose_msg)
             rate.sleep()
 
 if __name__ == '__main__':
