64eb33cb55c73e49cca3508a3358201751d0d088
Kim Hotvedt <kim.hotvedt1@outlook.com>
2026-04-14 14:33:23 +0200

    PI Controller

--- packages/mpc_runtime/src/PI_control_motors.py
+++ packages/mpc_runtime/src/PI_control_motors.py
@@ -118,8 +118,8 @@ class PI:
     """
 
     def __init__(self):
-        self.kp = float(0.05)
-        self.ki = float(0.10)
+        self.kp = float(0.5) # 0.05
+        self.ki = float(0.25) #0.10
         self.integral_limit = float(5.0)
         self.estimator = TickToVelocity()
         self.wheel_radius = self.estimator.wheel_radius
